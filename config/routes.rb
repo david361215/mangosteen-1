ebba427ca923b9c9827742ebb4649ebd60b7c575
david361215 <1138890119@qq.com>
2022-12-12 17:07:21 +0800

    创建路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      # /api/v1
+      resources :validation_codes, only: [:create] 
+      resources :session, only: [:create, :destroy]
+      resources :me, only: [:show]
+      resources :items
+      resources :tags 
+    end
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
