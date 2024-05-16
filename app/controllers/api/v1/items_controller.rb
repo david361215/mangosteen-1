f4b038316e1f4a357cca86e5f176ec2266ae70b6
david361215 <1138890119@qq.com>
2024-05-16 11:14:27 +0800

    完成创建账目 API 及其文档

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -11,7 +11,8 @@ class Api::V1::ItemsController < ApplicationController
     }}
   end
   def create
-    item = Item.new amount: params[:amount]
+    item = Item.new params.permit(:amount, :happened_at, tags_id: [] )   
+    item.user_id = request.env['current_user_id']    
     if item.save
       render json: {resource: item}
     else
