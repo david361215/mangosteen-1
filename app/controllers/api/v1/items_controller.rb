f87e1b116f12a1d9353fb1fbc340d0bca9524634
david361215 <1138890119@qq.com>
2024-05-16 14:43:39 +0800

    items 可以根据 happen_at 分组

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -19,4 +19,23 @@ class Api::V1::ItemsController < ApplicationController
       render json: {errors: item.errors}, status: :unprocessable_entity
     end
   end
+  def summary
+    hash = Hash.new
+    items = Item
+      .where(user_id: request.env['current_user_id'])
+      .where(kind: params[:kind])
+      .where(happened_at: params[:happened_after]..params[:happened_before])
+    items.each do |item|
+      key = item.happened_at.in_time_zone('Beijing').strftime('%F')
+      hash[key] ||= 0
+      hash[key] += item.amount
+    end
+    groups = hash
+      .map { |key, value| {"happened_at": key, amount: value} }
+      .sort { |a, b| a[:happened_at] <=> b[:happened_at] }
+    render json: {
+      groups: groups,
+      total: items.sum(:amount)
+    }
+  end
 end
