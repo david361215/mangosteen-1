64ba455f9dde1462d6ee8e57de6daa174ed8a6c5
david361215 <1138890119@qq.com>
2024-06-20 17:55:04 +0800

    后端默认格式不全的时间是北京时间

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,13 +3,15 @@ class Api::V1::ItemsController < ApplicationController
     current_user_id = request.env['current_user_id']
     return head :unauthorized if current_user_id.nil?
     items = Item.where(user_id: current_user_id)
-      .where(happened_at: params[:happened_after]..params[:happened_before])
+      .where(
+        happened_at: (datetime_with_zone(params[:happened_after])..datetime_with_zone(params[:happened_before]))
+      )    
     items = items.where(kind: params[:kind]) unless params[:kind].blank?
     items = items.page(params[:page])
     render json: { resources: items, pager: {
       page: params[:page] || 1,
       per_page: Item.default_per_page,
-      count: tags.count
+      count: items.count
     }}, methods: :tags
   end
   def create
--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -49,8 +49,9 @@ RSpec.describe "Items", type: :request do
     it "按时间筛选（边界条件2）" do
       item1 = create :item, happened_at: "2018-01-01"
       item2 = create :item, happened_at: "2017-01-01", user: item1.user
-
-      get '/api/v1/items?happened_after=2018-01-01', headers: item1.user.generate_auth_header
+      get "/api/v1/items", params:{
+        happened_after: '2018-01-01'
+      }, headers: item1.user.generate_auth_header
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq 1
@@ -97,7 +98,7 @@ RSpec.describe "Items", type: :request do
       expect(json['resource']['id']).to be_an(Numeric)
       expect(json['resource']['amount']).to eq 99
       expect(json['resource']['user_id']).to eq user.id
-      expect(json['resource']['happened_at']).to eq '2017-12-31T16:00:00.000Z'
+      expect(json["resource"]["happened_at"]).to eq "2018-01-01T00:00:00.000+08:00"
       expect(json["resource"]["kind"]).to eq "income"
     end
     it "创建时 amount、tag_ids、happened_at 必填" do
